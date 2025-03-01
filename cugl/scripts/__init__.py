6f8ce03e40e813299949afd524b0a15f5c67641a
pkc48 <pkc48@cornell.edu>
2025-03-01 13:48:34 -0500

    Removed cugl

--- cugl/scripts/__init__.py
+++
@@ -1,1 +0,0 @@
-# This file is necessary for some Python distributions
--- cugl/templates/android/__project__/app/jni/sd2pckg/Android.mk
+++
@@ -1,3 +0,0 @@
-# Top level NDK Makefile for Android application
-# We just invoke the subdirectories
-include $(call all-subdir-makefiles)
