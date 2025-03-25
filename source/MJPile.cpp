943151efe0e7a8eea5105171b785d4530066a4c7
pkc48 <pkc48@cornell.edu>
2025-03-25 19:15:47 -0400

    doafj

--- source/MJPile.cpp
+++ source/MJPile.cpp
@@ -194,10 +194,10 @@ void Pile::removePileTile(const std::shared_ptr<cugl::JsonValue> tileJson, bool 
         std::string key = rank + " of " + suit + " " + id;
         
         if(actionType != "None"){
-            key = actionType + " of action " + id;
+            key = actionType + id;
         }
         else if (commandType != "None") {
-            key = commandType + " of command " + id;
+            key = commandType + id;
         }
         
         int x = _pileMap[key].x;
