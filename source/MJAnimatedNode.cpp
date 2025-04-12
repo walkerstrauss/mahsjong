8448fe48b4050d50b39d6fcfa060006b5915da32
was236 <was236@cornell.edu>
2025-04-12 15:50:51 -0400

    Implemented animated node

--- source/MJAnimatedNode.cpp
+++ source/MJAnimatedNode.cpp
@@ -5,3 +5,108 @@
 //  Created by Walker Strauss on 4/9/25.
 //
 
+#include "MJAnimatedNode.h"
+
+using namespace cugl;
+using namespace cugl::scene2;
+using namespace cugl::graphics;
+
+AnimatedNode::AnimatedNode():
+_rows(0), _cols(0), _frame(0), _limit(0), _timeSinceFrameAdvance(0.0f),_isPlaying(false),_isInterrupting(false) {}
+
+AnimatedNode::~AnimatedNode() {}
+
+bool AnimatedNode::initWithSheet(const std::shared_ptr<Texture>& texture, int rows, int cols){
+    CUAssertLog(texture, "Texture must not be null");
+    _texture = texture;
+    return true;
+}
+
+bool AnimatedNode::initWithData(const AssetManager* assets, const std::shared_ptr<JsonValue>& json, float fps){
+    CUAssertLog(json, "JsonValue must not be null");
+    
+    // Load all idle animations from the JSON
+    auto idleAnims = json->get("idle");
+    for (auto animJson : idleAnims->children()){
+        Animation a;
+        a.init(AnimationType::IDLE, animJson->getString("key"), animJson->getInt("rows"),animJson->getInt("cols"),animJson->getInt("count"), fps, true);
+        _idleAnims[a.key] = a;
+    }
+    
+    // Get default idle animation from JSON – if present
+    if (idleAnims->has("default")){
+        setDefaultIdleKey(idleAnims->getString("default"));
+    }
+    
+    // Load all interrupting animations from the JSON
+    auto interruptAnims = json->get("interrupt");
+    for (auto animJson : interruptAnims->children()){
+        Animation a;
+        a.init(AnimationType::INTERRUPT, animJson->getString("key"), animJson->getInt("rows"), animJson->getInt("cols"), animJson->getInt("count"), fps, false);
+        _interruptAnims[a.key] = a;
+    }
+    return true;
+}
+
+void AnimatedNode::play(const std::string& key, AnimationType type){
+    const auto& animMap = (type == AnimationType::IDLE) ? _idleAnims : _interruptAnims;
+    CUAssertLog(animMap.find(key) != animMap.end(), "Animation with key '%s' not found", key.c_str());
+    
+    _currKey = key;
+    _currAnim = animMap.at(key);
+    _timeSinceFrameAdvance = 0.0f;
+    _isPlaying = true;
+    _isInterrupting = (type == INTERRUPT);
+    _currAnim.done = false;
+    setFrame(_frame);
+    return;
+}
+
+void AnimatedNode::stop(){
+    _isPlaying = false;
+    _isInterrupting = false;
+}
+
+void AnimatedNode::update(float timestep){
+    if (!_isPlaying) return;
+    _timeSinceFrameAdvance += timestep;
+    
+    float frameDur = 1.0f / _currAnim.fps;
+    if (_timeSinceFrameAdvance >= frameDur){
+        _timeSinceFrameAdvance -= frameDur;
+        _frame++;
+        
+        if (_frame > _currAnim.endFrame) {
+            if (_currAnim.loop && !_isInterrupting){
+                _frame = _currAnim.startFrame;
+            } else {
+                _isPlaying = false;
+                _currAnim.done = true;
+                
+                if (_isInterrupting) {
+                    _isInterrupting = false;
+                    if (!_defaultIdleKey.empty()){
+                        play(_defaultIdleKey, IDLE);
+                    }
+                }
+                
+                return;
+            }
+        }
+        
+        setFrame(_frame);
+    }
+}
+
+void AnimatedNode::setFrame(int frame){
+    CUAssertLog(frame >= 0 && frame < _limit, "Invalid animation frame %d", frame);
+    
+    _frame = frame;
+    float x = (frame % _cols)*_bounds.size.width;
+    float y = _texture->getSize().height - (1+frame/_cols)*_bounds.size.height;
+    float dx = x-_bounds.origin.x;
+    float dy = y-_bounds.origin.y;
+    _bounds.origin.set(x,y);
+    shiftTexture(dx, dy);
+}
+
--- source/MJAnimatedNode.h
+++ source/MJAnimatedNode.h
@@ -7,5 +7,84 @@
 
 #ifndef __MJ_ANIMATED_NODE_H__
 #define __MJ_ANIMATED_NODE_H__
+#include <cugl/scene2/CUPolygonNode.h>
+#include <cugl/core/math/CURect.h>
+#include <cugl/graphics/CUTexture.h>
+#include <unordered_map>
 
-class
+using namespace cugl;
+using namespace cugl::scene2;
+using namespace cugl::graphics;
+
+/**
+ * Class representing an animated scene node in the game – all tiles will be animated nodes by open beta
+ */
+class AnimatedNode : public PolygonNode {
+public:
+    enum AnimationType {
+        IDLE,
+        INTERRUPT
+    };
+    
+protected:
+    struct Animation {
+        AnimationType type;
+        std::string key;
+        int rows, cols, startFrame, endFrame;
+        float fps;
+        bool loop, done;
+        
+        bool init (AnimationType type, std::string key, int rows, int cols, int endFrame, float fps, bool loop){
+            type = type;
+            key = key;
+            rows = rows;
+            cols = cols;
+            startFrame = 0;
+            endFrame = endFrame;
+            fps = fps;
+            loop = loop;
+            done = false;
+            return true;
+        }
+    };
+    
+    std::unordered_map<std::string, Animation> _idleAnims, _interruptAnims;
+    std::string _currKey;
+    Animation _currAnim;
+    float _timeSinceFrameAdvance;
+    int _rows, _cols;
+    int _frame, _limit;
+    bool _isPlaying, _isInterrupting;
+    Rect _bounds;
+    std::string _defaultIdleKey;
+public:
+    /**
+     * Default constructor
+     **/
+    AnimatedNode();
+    
+    /**
+     * Destructor
+     **/
+    ~AnimatedNode();
+    
+    bool initWithSheet(const std::shared_ptr<Texture>&, int rows, int cols);
+    
+    bool initWithData(const AssetManager* assets, const std::shared_ptr<JsonValue>& json, float fps);
+    
+    void play(const std::string& key, AnimationType type);
+    
+    void stop();
+    
+    void update(float dt);
+    
+    bool isAnimating() const { return _isPlaying; }
+    
+    const std::string& getCurrentKey() const { return _currKey; }
+    
+    void setFrame(int frame);
+    
+    void setDefaultIdleKey(const std::string& key) { _defaultIdleKey = key; }
+};
+
+#endif
