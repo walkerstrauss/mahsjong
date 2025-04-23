99d7164dacb73f6076517a594d3532d5bfede68a
Tina <tl493@cornell.edu>
2025-04-22 21:40:19 -0400

    Delete build directory

--- build/cmake/cmake/cugl.dir/external/libdatachannel/deps/openssl/crypto/buildinf.h
+++
@@ -1,6 +0,0 @@
-/* auto-generated for crypto/cversion.c */
-#define CFLAGS "compiler: AppleClang -fPIC"
-#define PLATFORM "platform: Darwin"
-#define DATE "built on: 2025-04-10 19:56:08 UTC"
-static const char *compiler_flags = CFLAGS;
-
