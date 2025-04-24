6fd16e92b1e9ca1f5359298bff06c0ad3b07fcb7
pkc48 <pkc48@cornell.edu>
2025-04-23 22:52:26 -0400

    Delete build directory

--- build/cmake/cmake/cugl.dir/CMakeFiles/cugl-core.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for cugl-core.
-# This may be replaced when dependencies are built.
--- build/cmake/cmake/cugl.dir/sdl2pckg.dir/sdl2/CMakeFiles/CheckOBJC/result.cmake
+++
@@ -1,1 +0,0 @@
-set(CMAKE_OBJC_COMPILER "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/cc")
